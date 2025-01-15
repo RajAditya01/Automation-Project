7687ba4fc45db97594765deaa0befa8531bc5b5d
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2025-01-16 02:18:09 +0530

    Technique To Automate Ajax call child Window and Iframes

---
+++ AutomateAjaxCallChildWindow/src/main/java/WIndowHandles.java
@@ -0,0 +1,59 @@
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class WindowHandleConcepts {
+//    System.setProperty("","");
+//    WebDriver driver = new ChromeDriver();
+//
+//    driver.get();
+//    driver.findElement(By.cssSelector("")).click();
+//    Set<String> windows=driver.getWindowHandles();
+//    String parentId = it.next();
+//    String childId = it.next();
+//    driver.switchTo().window(childId);
+//    System.out.println(driver.findElement(By.cssSelector(".im-param.red")).getText());
+//}
+
+
+
+
+
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WIndowHandles {
+
+
+    public static void main(String[] args) {
+
+        // Setting up WebDriver for Chrome
+        System.setProperty("webdriver.chrome.driver", "C:\\BrowserDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+        Set<String> windows = driver.getWindowHandles(); //[parentid,childid,subchildId]
+        Iterator<String> it = windows.iterator();
+
+        String parentId = it.next();
+
+        String childId = it.next();
+
+        driver.switchTo().window(childId);
+
+        System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+
+        driver.findElement(By.cssSelector(".im-para.red")).getText();
+
+        String emailId = driver.findElement(By.cssSelector(".im-para.red")).getText().split("at")[1].trim().split(" ")[0];
+
+        driver.switchTo().window(parentId);
+
+        driver.findElement(By.id("username")).sendKeys(emailId);
+
+    }
+}
--- AutomateAjaxCallChildWindow/src/main/java/org/example/App.java
+++ AutomateAjaxCallChildWindow/src/main/java/org/example/App.java
@@ -1,13 +1,39 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class App {
+    public static void main(String[] args) {
+        // Setting up WebDriver for Chrome
+        System.setProperty("webdriver.chrome.driver", "C:\\BrowserDriver\\chromedriver.exe");
+
+        // Initializing WebDriver
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            // Navigate to Amazon India website
+            driver.get("https://www.amazon.in/");
+
+            // Create an Actions object
+            Actions actions = new Actions(driver);
+
+            // Move to the search box element
+            WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+            actions.moveToElement(searchBox).perform();
+
+            // Move to the account list menu element
+            WebElement accountList = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+            actions.moveToElement(accountList).perform();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close the browser
+//            driver.quit();
+        }
     }
 }
